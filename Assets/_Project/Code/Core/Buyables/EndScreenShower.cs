38246a4bce60db83a7eeeae6b35fb7f567536bb3
NeznauNeznau <61615410+NeznauNeznau@users.noreply.github.com>
2025-04-06 11:31:15 +0300

    Start
    Start

---
+++ Assets/_Project/Code/Core/Buyables/EndScreenShower.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Project.Code.Core.Character;
+using _Project.Code.Entry;
+using _Project.Code.Services.Input;
+using UnityEngine;
+
+namespace _Project.Code.Core.Buyables
+{
+    public class EndScreenShower : MonoBehaviour
+    {
+        [SerializeField] private Buyable _fieldBuyable;
+        [SerializeField] private GameObject _ui;
+        [SerializeField] private Mover _moveInput;
+        [SerializeField] private GameObject joystick;
+
+        private bool _isFinished;
+        private Coroutine timerCoroutine;
+        private float timer = 0f;
+
+
+        private void Update()
+        {
+            if(_moveInput.isMoving)
+            {
+            }
+
+            if (_isFinished) return;
+
+            if (_fieldBuyable.IsBought)
+            {
+                _isFinished = true;
+                StartCoroutine(EndGame());
+            }
+
+
+
+            if (_moveInput.isMoving)
+            {
+                // Сброс таймера при движении
+                if (timerCoroutine != null)
+                {
+                    StopCoroutine(timerCoroutine);
+                    timerCoroutine = null;
+                    timer = 0f; // Сброс времени
+                }
+            }
+            else
+            {
+                // Запуск или продолжение таймера при остановке
+                if (timerCoroutine == null)
+                {
+                    timerCoroutine = StartCoroutine(CountdownTimer());
+                }
+            }
+        }
+
+        private IEnumerator EndGame()
+        {
+            yield return new WaitForSeconds(3f);
+
+            _moveInput._speed = 0f;
+            joystick.SetActive(false);
+            _ui.SetActive(true);
+        }
+
+        private IEnumerator CountdownTimer()
+        {
+            timer = 0f; // Сброс времени при запуске корутины
+            while (timer < 5f)
+            {
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            _isFinished = true;
+            StartCoroutine(EndGame());
+        }
+    }
+}
---
+++ Assets/_Project/Code/Core/Character/Mover.cs
@@ -0,0 +1,53 @@
+using System;
+using _Project.Code.Services.Input;
+using UnityEngine;
+
+namespace _Project.Code.Core.Character
+{
+    public class Mover : MonoBehaviour 
+    {
+        [SerializeField] public float _speed = 10f;
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        private const float SpeedScaleFactor = 0.1f;
+
+        private IMoveInput _input;
+
+        public Vector2 Direction => _input.Direction;
+
+        private Vector2 previousPosition;
+
+        public bool isMoving;
+
+        private void Start()
+        {
+            previousPosition = _rigidbody.position;
+        }
+
+        public void Init(IMoveInput input)
+        {
+            _input = input;
+        }
+
+
+        private void FixedUpdate()
+        {
+            if (_input is null) _input = new KeyboardMoveInput();
+
+            var targetPosition = _rigidbody.position + Direction * (_speed * SpeedScaleFactor * Time.fixedDeltaTime);
+            
+            _rigidbody.MovePosition(targetPosition);
+
+
+
+
+
+
+            var targetPosition_2 = _rigidbody.position + Direction * (_speed * SpeedScaleFactor * Time.fixedDeltaTime);
+            _rigidbody.MovePosition(targetPosition_2);
+
+            isMoving = _rigidbody.position != previousPosition;
+            previousPosition = _rigidbody.position;
+        }
+    }
+}
